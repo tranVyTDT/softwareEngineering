438d0eedc11405f7218b9ea59cede55cc8cea8db
tranVyTDT <40004136+tranVyTDT@users.noreply.github.com>
2018-11-10 00:59:57 +0700

    add description
    add description

--- homeWork/MyBigNumber.java
+++ homeWork/MyBigNumber.java
@@ -11,12 +11,12 @@
 class MyBigNumber {
     public String sum(String s1 , String s2)
 	{
-		int balance = 0; 
-                char value1 , value2;
-		int maxLenght = s1.length() > s2.length() ? s1.length() : s2.length();
-		String result = "";
+		int balance = 0; // = 1 if plus 2 number > 9 else 0
+                char value1 , value2; //use to keep single number
+		int maxLenght = s1.length() > s2.length() ? s1.length() : s2.length(); // if lenght's string 1 > lenght's string 2 then maxstring = lenght's string1 else  lenght's string2
+		String result = "";// use to keep result of this function;
 		
-		for(int i = 0 ; i < maxLenght ; i++) 
+		for(int i = 0 ; i < maxLenght ; i++) //carry out add 2 number
 		{
                         if(s1.length() - i-1 < 0)
                         {
